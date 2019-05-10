f737e250e698fcc6330ae8f9c5c21681b147dd9e
dannyaudi <50224134+dannyaudi@users.noreply.github.com>
2019-05-10 19:35:21 -0400

    Add files via upload

---
+++ combineAudio.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/wav"
+)
+
+// This function receives a slice of the paths for all audio clips to be combined to form a word.
+// It iterates over the list, combining the next clip with the base clip, or "streamer".
+// The file is written to the same directory under the name "output.wav"
+
+func combineAudio(paths []string) {
+
+	f, _ := os.Open(paths[0])
+	streamerBase, format, _ := wav.Decode(f)
+	defer streamerBase.Close()
+
+	streamerFinal := beep.Seq(streamerBase)
+
+	for i := 1; i < len(paths); i++ {
+
+		g, _ := os.Open(paths[i])
+
+		streamerTemp, _, _ := wav.Decode(g)
+
+		streamerFinal = beep.Seq(streamerFinal, streamerTemp)
+	}
+
+	writer, _ := os.Create("output.wav")
+
+	wav.Encode(writer, streamerFinal, format)
+}
